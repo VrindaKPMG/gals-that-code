7f5ba7dfc923a8de3a0b2e58e63570197d8d22fd
VrindaKPMG <vrinda.gera@kpmg.co.uk>
2024-09-15 14:44:33 +0100

    Task 1: Adding animal classes

---
+++ tasks/1-virtual-zoo/animal_class.py
@@ -0,0 +1,17 @@
+class Lion:
+    def __init__(self, species, name, age):
+        self.species = species
+        self.name = name
+        self.age = age
+        
+class Elephant:
+    def __init__(self, species, name, age):
+        self.species = species
+        self.name = name
+        self.age = age
+        
+class Tiger:
+    def __init__(self, species, name, age):
+        self.species = species
+        self.name = name
+        self.age = age
